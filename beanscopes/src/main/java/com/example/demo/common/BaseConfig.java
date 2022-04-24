f22abdce9bba5c74b6878cf561a94510ad7ac9b3
osinskja <osinskja@gmail.com>
2022-04-25 00:34:01 +0200

    example from Netcompany

--- beanscopes/src/main/java/com/example/demo/common/BaseConfig.java
+++ beanscopes/src/main/java/com/example/demo/common/BaseConfig.java
@@ -6,10 +6,15 @@ import com.example.demo.connector.person.IntegrationPersonSftpConnector;
 import com.example.demo.connector.person.IntegrationPersonSftpConnectorImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
 
 @Configuration
 public class BaseConfig {
     @Bean
+    @Scope(scopeName = SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
     public SftpFileUploader sftpFileUploader() {
         return new SftpFileUploader();
     }
